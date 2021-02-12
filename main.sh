d6da357e1c39df29e68b2ae1e882bcd7c4f6c42d
Alvin Seville <alvinseville7cf@gmail.com>
2021-02-12 16:03:50 +0000

    shebang added

--- main.sh
+++ main.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 list=("using x" "using x1" "using 1x" "using static" "using static x1" "using static 1x" "using x=y" "using x1=y" "using 1x=y" "using x=y1" "using x1=y1" "using 1x=y1" "using x=1y" "using x1=1y" "using 1x=1y" "using static x=y" "using static x1=y" "using static 1x=y" "using static x=y1" "using static x1=y1" "using static 1x=y1" "using static x=1y" "using static x1=1y" "using static 1x=1y")
 
 for i in "${list[@]}"
